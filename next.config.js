191a0b54cd70e9838404ef75e26226589aad8e97
GCodeON <gcodeondev@gmail.com>
2023-09-09 02:26:32 -0700

    config clean up

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    env: {
-        SPOTIFY_CLIENT_ID    : "0b9ef3c3ea8c44959cf5b6a9b5fe71d9",
-        SPOTIFY_CLIENT_SECRET: "3e1020b4edfb436086a4776546b71e62",
-        SPOTIFY_REDIRECT_URI : "http://localhost:3000/callback/"
-      },
+  env: {
+      SPOTIFY_CLIENT_ID    : process.env.SPOTIFY_CLIENT_ID,
+      SPOTIFY_CLIENT_SECRET: process.env.CLIENT_SECRET,
+      SPOTIFY_REDIRECT_URI :  process.env.SPOTIFY_REDIRECT_URI
+  },
 }
 
 module.exports = nextConfig
