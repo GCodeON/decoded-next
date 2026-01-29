0ff7a978cf0397d98f01edc1c8620a315c9d828c
GCodeON <gcodeondev@gmail.com>
2026-01-29 12:50:05 -0800

    fix suspense boundary

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata, Viewport } from 'next';
 import { Inter } from 'next/font/google';
+import { Suspense } from 'react';
 import ClientProviders from '@/components/ClientProviders';
 
 const inter = Inter({ subsets: ['latin'] })
@@ -18,9 +19,11 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang="en">
       <body className={`${inter.className}`}>
-        <ClientProviders>
-          {children}
-        </ClientProviders>
+        <Suspense fallback={<div />}>
+          <ClientProviders>
+            {children}
+          </ClientProviders>
+        </Suspense>
       </body>
     </html>
   )
